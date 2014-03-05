1f0b506e0db315bfa1785da08dbccd6e9d29b3e3
Aries McRae <aries@ariesmcrae.com>
2014-03-05 22:03:39 +1100

    Added partials

---
+++ public/js/airportApp.js
@@ -0,0 +1,12 @@
+var airportApp = angular.module('airportApp', [])
+
+airportApp.config(airlineRouter);
+
+function airlineRouter($routeProvider) {
+	$routeProvider
+		.when('/', {templateUrl:'partials/destinations.html'})
+		.when('/flights', {template:'<h3>Flights</h3>'})
+		.when('/reservations', {template: '<h3>Your Reservations</h3>'});
+}
+
+
